2c21fe5384b0a74c1b5f823414de15a5db35a2ba
Zachary Walker <walker@Zacharys-MacBook-Pro.local>
2022-02-01 17:14:52 -0500

    nodemailer test

--- mailer.js
+++ mailer.js
@@ -5,6 +5,9 @@ const { EMAIL, PASSWORD } = require('./config')
 const nodemailer = require("nodemailer");
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD
